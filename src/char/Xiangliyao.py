4f9f6b24d1c1f26f23ede85861bec77c91b175ad
John Ebona <j.ebona5084@student.sbccd.edu>
2026-06-12 02:32:49 -0700

    Encode Calcharo/Xiangli Yao/Yinlin electro rotation with quickswaps and cancels (#4)
    Calcharo: Deathblade Gear loop (5 basics -> Death Messenger with dodge cancels), slot-start echo for the Hyvatia relay, MUST after Yinlin, exit top-offs. Yinlin: liberation last for the swap-cancel and fresh outro deepen. Xiangli Yao: MUST after Calcharo, filler top-offs.

--- src/char/Xiangliyao.py
+++ src/char/Xiangliyao.py
@@ -1,6 +1,6 @@
 import time
 
-from src.char.BaseChar import BaseChar
+from src.char.BaseChar import BaseChar, SwitchPriority
 
 
 class Xiangliyao(BaseChar):
@@ -9,18 +9,27 @@ class Xiangliyao(BaseChar):
         super().__init__(*args, **kwargs)
         self.liberation_time = 0
 
+    def get_switch_priority(self, current_char=None, has_intro=False, target_low_con=False):
+        if has_intro and current_char and current_char.char_name in {'char_calcharo'}:
+            return SwitchPriority.MUST
+        return super().get_switch_priority(current_char, has_intro, target_low_con)
+
     def do_perform(self):
         self.wait_down()
         if self.click_liberation():
             self.liberation_time = time.time()
         if self.still_in_liberation():
+            # the window-ending resonance flows straight into the switch,
+            # which cancels its recovery; no top-off here to keep it tight
             while not self.click_resonance(send_click=True)[0]:
                 self.continues_normal_attack(1)
-        elif self.echo_available():
-            self.logger.debug('click_echo')
-            self.click_echo()
         else:
-            self.click_resonance(send_click=True)
+            if self.echo_available():
+                self.logger.debug('click_echo')
+                self.click_echo()
+            else:
+                self.click_resonance(send_click=True)
+            self.top_off_con()
         self.switch_next_char()
 
     def still_in_liberation(self):
